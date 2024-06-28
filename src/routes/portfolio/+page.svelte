e2f9c0102349927cde71f419f95abe298cc028d7
Mauro Cigliuti <mcigliuti01@gmail.com>
2024-06-28 19:57:21 -0300

    add

---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Layout from '../+layout.svelte';
+</script>
+
+<div class="flex h-full w-full items-center justify-center bg-black text-7xl">
+	<h1 class="text-white">Portfolio</h1>
+</div>
